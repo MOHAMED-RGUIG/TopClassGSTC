e17e0bb64e038a0921b441c772a2bc740a96bdaa
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-11-14 12:17:23 +0100

    Update tables.jsx

--- src/pages/dashboard/tables.jsx
+++ src/pages/dashboard/tables.jsx
@@ -359,7 +359,7 @@ export function Tables() {
    
 
 {/* FILTRE */}
-      <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-1 bg-[#ECEFF100]">
+      <div className="mb-1 grid grid-cols-1 gap-6 xl:grid-cols-1 bg-[#ECEFF100]">
       {/* Search and Filter Controls */}
       <div className="flex items-center gap-4 mb-4">
         <input
